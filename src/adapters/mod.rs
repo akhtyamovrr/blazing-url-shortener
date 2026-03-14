4a429a594d0cb1239d96bdbb2596ea4e032de409
Ruslan Akhtyamov <akhtyamovrr@Ruslans-MacBook-Pro-2.local>
2026-03-15 01:49:40 +0300

    naive PG implementation

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -3,14 +3,14 @@ use std::sync::Arc;
 use async_trait::async_trait;
 
 use dashmap::DashMap;
-
+use deadpool_postgres::Pool;
 
 #[async_trait]
 pub trait CreateShortLinkRepository: Send + Sync {
     async fn save(&self, id: String, full_url: String) -> Result<(), String>;
 }
 
-#[async_trait] 
+#[async_trait]
 pub trait QueryFullUrlRepository: Send + Sync {
     async fn get_full_url_by_id(&self, id: String) -> Result<String, String>;
 }
@@ -26,7 +26,7 @@ impl InmemoryRepository {
     }
 }
 
-#[async_trait] 
+#[async_trait]
 impl CreateShortLinkRepository for InmemoryRepository {
     async fn save(&self, id: String, full_url: String) -> Result<(), String> {
         self.storage.insert(id, full_url);
@@ -41,5 +41,44 @@ impl QueryFullUrlRepository for InmemoryRepository {
             Some(v) => Ok(v.clone()),
             None => Err("no such key".to_owned()),
         }
+    }
+}
+
+pub struct PgRepository {
+    pool: Pool,
+}
+
+impl PgRepository {
+    pub fn new(pool: Pool) -> Self {
+        PgRepository { pool }
+    }
+}
+
+#[async_trait]
+impl CreateShortLinkRepository for PgRepository {
+    async fn save(&self, id: String, full_url: String) -> Result<(), String> {
+        let client = self.pool.get().await.unwrap();
+        client
+            .execute(
+                "INSERT INTO links (id, full_url) VALUES ($1, $2)",
+                &[&id, &full_url],
+            )
+            .await
+            .unwrap();
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl QueryFullUrlRepository for PgRepository {
+    async fn get_full_url_by_id(&self, id: String) -> Result<String, String> {
+        let client = self.pool.get().await.unwrap();
+        let stmt = client
+            .prepare("SELECT full_url FROM links where id = $1")
+            .await
+            .unwrap();
+        let row = client.query_one(&stmt, &[&id]).await.unwrap();
+        let value = row.get(0);
+        return Ok(value);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
 use crate::{
-    adapters::InmemoryRepository, id_provider::NanoIdProvider, ports::rest::{Container, Server}
+    adapters::PgRepository, id_provider::NanoIdProvider, ports::rest::{Container, Server}
 };
-use dashmap::DashMap;
+
+use deadpool_postgres::{Config, Runtime};
+use tokio_postgres::NoTls;
 
 mod adapters;
 mod app;
@@ -13,18 +15,21 @@ mod ports;
 #[tokio::main]
 async fn main() {
     let id_provider = NanoIdProvider;
-    let storage = Arc::new(DashMap::new());
-    let create_short_link_repo = InmemoryRepository::new(storage.clone());
-    let query_full_url_repo = InmemoryRepository::new(storage.clone());
+    // let storage = Arc::new(DashMap::new());
+    // let create_short_link_repo = InmemoryRepository::new(storage.clone());
+    // let query_full_url_repo = InmemoryRepository::new(storage.clone());
 
-    // let mut cfg = Config::new();
-    // cfg.host = Some("localhost".to_string());
-    // cfg.user = Some("postgres".to_string());
-    // cfg.dbname = Some("test_db".to_string());
-    // let pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+    let mut cfg = Config::new();
+    cfg.host = Some("localhost".to_string());
+    cfg.user = Some("postgres".to_string());
+    cfg.dbname = Some("test_db".to_string());
+    cfg.password = Some("mysecretpassword".to_string());
 
-    // let create_short_link_repo = PgRepository::new(pool.clone());
-    // let query_full_url_repo = PgRepository::new(pool.clone());
+    let pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+
+
+    let create_short_link_repo = PgRepository::new(pool.clone());
+    let query_full_url_repo = PgRepository::new(pool.clone());
 
     let container = Arc::new(Container::new(
         id_provider,
